2bbc34ee4e04071be28ada150621a7c04da88fb5
felix.larrue <felixlarrue@gmail.com>
2022-02-21 17:34:42 +0100

    format output message and time sequential function

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h> // for timing
 
 #define N 1000000
+
+double now()
+{
+    // Retourne l'heure actuelle en secondes
+    struct timeval t;
+    double f_t;
+    gettimeofday(&t, NULL);
+    f_t = t.tv_usec;
+    f_t = f_t / 1000000.0;
+    f_t += t.tv_sec;
+    return f_t;
+}
 
 double rnorm(float *U, int n)
 {
@@ -25,7 +39,17 @@ void fill_array(float *buf, int n)
 int main()
 {
     float U[N] = {0.0};
+    double res_seq;
+    double time_seq;
+
+    time_seq = now();
     fill_array(U, N);
-    float res = rnorm(U, 5);
-    printf("Res is %lf", res);
+    time_seq = now() - time_seq;
+
+    res_seq = rnorm(U, 5);
+    printf("VALEURS\n");
+    printf("Sequentiel (scalaire: %lf vectoriel: ...) Parallèle (nb_thread: ... scalaire ... vectoriel ...)\n", res_seq);
+    printf("TEMPS D'EXECUTION\n");
+    printf("Sequentiel (scalaire: %lf vectoriel: ...) Parallèle (nb_thread: ... scalaire ... vectoriel ...)\n", time_seq);
+    printf("Accélération (vectoriel: ... multithread: ... vectoriel + multithread ...)\n");
 }
